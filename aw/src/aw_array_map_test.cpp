49e737f070de9e6530b10035e2a5a577fb7216ea
Christopher Ariza <flexatone@users.noreply.github.com>
2012-09-19 09:41:37 -0700

    updated makefile, made consolidated boost testing work

--- aw/src/aw_array_map_test.cpp
+++ aw/src/aw_array_map_test.cpp
@@ -1,19 +1,26 @@
-// g++ aw_array_map_test.cpp aw_array_map.cpp aw_common.cpp -Wall -o aw_array_map_test
+// g++ aw_array_map_test.cpp aw_array_map.cpp aw_common.cpp -std=c++0x -Wall -o aw_array_map_test
 
 // g++ -c aw_common.cpp -Wall 
 
-#define BOOST_TEST_MODULE
 
+#define BOOST_TEST_DYN_LINK
+#ifdef STAND_ALONE
+#   define BOOST_TEST_MODULE main
+#endif
+#include <boost/test/unit_test.hpp>
 
-#include <boost/test/included/unit_test.hpp>
-#include <boost/test/included/unit_test_framework.hpp> 
+// stand-alone way of testing with non-compiled boost
+//#define BOOST_TEST_MODULE
+//#include <boost/test/included/unit_test.hpp>
+//#include <boost/test/included/unit_test_framework.hpp> 
 
 #include "aw_common.h"
 #include "aw_array_map.h"
 
 
 // provide testCase, name
-BOOST_AUTO_TEST_CASE(Basic) {
+BOOST_AUTO_TEST_CASE(aw_array_map_test1) {
+	std::cout << "running aw_array_map_test" << std::endl;
 
 	// this has to be const
 	aw::AST const size = 12;
@@ -26,3 +33,4 @@ BOOST_AUTO_TEST_CASE(Basic) {
 
 
 }
+
--- aw/src/aw_common_test.cpp
+++ aw/src/aw_common_test.cpp
@@ -1,18 +1,26 @@
-// g++ aw_common_test.cpp aw_common.cpp -Wall -o aw_common_test
-// g++ -c aw_common.cpp -Wall 
-
-#define BOOST_TEST_MODULE
+// g++ aw_common_test.cpp aw_common.cpp -DSTAND_ALONE -lboost_unit_test_framework -std=c++0x -Wall -o aw_common_test
 
 
-#include <boost/test/included/unit_test.hpp>
-#include <boost/test/included/unit_test_framework.hpp> 
+
+#define BOOST_TEST_DYN_LINK
+#ifdef STAND_ALONE
+#   define BOOST_TEST_MODULE main
+#endif
+#include <boost/test/unit_test.hpp>
+
+// stand-alone way of testing with non-compiled boost
+//#define BOOST_TEST_MODULE
+//#include <boost/test/included/unit_test.hpp>
+//#include <boost/test/included/unit_test_framework.hpp> 
+
+#include <iostream>
 
 #include "aw_common.h"
 
 
 // provide testCase, name
-BOOST_AUTO_TEST_CASE(Basic) {
-
+BOOST_AUTO_TEST_CASE(aw_common_test1) {
+	std::cout << "running aw_common_test 1" << std::endl;
 	double val; 
 	val = aw::denormalize_unit(0.5, 0, .5);
     BOOST_CHECK_EQUAL(val, 0.25);
@@ -24,3 +32,14 @@ BOOST_AUTO_TEST_CASE(Basic) {
     BOOST_CHECK_EQUAL(val, 1);    
 
 }
+
+BOOST_AUTO_TEST_CASE(aw_common_test2) {
+	std::cout << "running aw_common_test 2" << std::endl;
+	double val; 
+
+	val = aw::denormalize_unit(0.5, 0, 2);
+    BOOST_CHECK_EQUAL(val, 1);
+    
+
+}
+
