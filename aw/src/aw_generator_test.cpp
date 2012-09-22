bacbff5d4136d8fd676d93ed0205967f81d90a04
Christopher Ariza <flexatone@users.noreply.github.com>
2012-09-21 22:01:24 -0700

    added ParameterType class

--- aw/src/aw_generator_test.cpp
+++ aw/src/aw_generator_test.cpp
@@ -1,5 +1,5 @@
-// g++ aw_generator_test.cpp aw_generator.cpp aw_common.cpp -DSTAND_ALONE -lboost_unit_test_framework -std=c++0x -Wall -o aw_generator_test
-
+// g++ aw_generator_test.cpp aw_generator.cpp aw_common.cpp -DSTAND_ALONE -lboost_unit_test_framework  -Wall -o aw_generator_test
+// -std=c++0x
 
 #define BOOST_TEST_DYN_LINK
 #ifdef STAND_ALONE
@@ -26,6 +26,15 @@ BOOST_AUTO_TEST_CASE(aw_generator_test_1) {
 	g2->render(4);
 	g2->print_output();
 
+
+	// can creat the table subclass
+	aw::Constant g3;
+	
+	
+	//==========================================================================
+	// parameter type tests
+	aw::ParameterType pt1;
+	pt1.index = 3;
 }
 
 
