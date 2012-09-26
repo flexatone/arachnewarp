621e5533922110f6c5f4442a53e676156541e5ac
Christopher Ariza <flexatone@users.noreply.github.com>
2012-09-26 11:27:14 -0700

    preliminary implementation of Add gen

--- aw/src/aw_generator.cpp
+++ aw/src/aw_generator.cpp
@@ -14,6 +14,11 @@ ParameterType :: ParameterType() {
 ParameterType :: ~ParameterType() {
 }
 
+std::ostream& operator<<(std::ostream& output, const ParameterType& pt) {
+    output << "<PmtrType: " << pt._name << ">";
+    return output; 
+}
+
 
 
 //==============================================================================
@@ -32,18 +37,17 @@ Generator :: Generator()
 
 // dtor
 Generator :: ~Generator() {
-    delete output; // remove dyn alloc memeory
+    delete[] output; // remove dyn alloc memeory
 }
 
 void Generator :: _init() {
-    // in derived classes this can prepare parameters
     std::cout << *this << ": init()" << std::endl;
 }
 
 void Generator :: _resize_output() {
     std::cout << "calling " << *this << " _resize_output" << std::endl;
     // must delete if set to something else
-    if (output != NULL) delete output;
+    if (output != NULL) delete[] output;
     // size is always dim * _frame_size
     // only do this once to avoid repeating
     _output_size = _output_frame_dimension * _frame_size;
@@ -60,45 +64,45 @@ void Generator :: reset() {
     _frame_count = 0;
 }
 
-std::ostream &operator<<(std::ostream& output, const Generator& g) {
+std::ostream& operator<<(std::ostream& output, const Generator& g) {
     output << "<Gen: " << g._name << ">";
     return output; 
 }
 
 void Generator :: print_output() {
     // provide name of generator first by dereferencing this
-    std::cout << *this << std::endl;
-    // TODO: estimate string size for uniform spacing
-    std::cout << "<output@" << _frame_count << ": ";            
+    std::cout << *this << " Output@" << _frame_count << ": ";
     for (int i=0; i<_output_size; ++i) {
-        if (i % _frame_size == 0 and i != 0) {
-            std::cout << std::endl << "           ";        
+        if (i % _frame_size == 0) {
+            std::cout << std::endl << "    ";        
         }
         std::cout << std::setprecision(8) << output[i] << '|';
     }
-    std::cout << '>' << std::endl;
+    std::cout << std::endl;
 }
 
 void Generator :: print_inputs() {
-    // provide name of generator first by dereferencing this
-    std::cout << *this << " inputs:" << std::endl;
-
-    VVGeneratorShared :: const_iterator k;
+    std::cout << *this << " Inputs:" << std::endl;
     VGeneratorShared :: const_iterator j;
-
-    for (k=inputs.begin(); k!=inputs.end(); ++k) {
+    // need an interger as key for _input_parameter_type
+    for (PARAMETER_INDEX_T k=0; k!=_inputs.size(); ++k) {   
+        ParameterTypeShared pts = _input_parameter_type[k];
         // need to iterate over each sub vector
-        std::cout << "    " << "input" << std::endl;
-        VGeneratorShared sub = *k; // get vector handle
-        for (j=sub.begin(); j!=sub.end(); ++j) {
+        std::cout << "    " << *pts << std::endl;
+        for (j=_inputs[k].begin(); j!=_inputs[k].end(); ++j) {
             // deref
             GeneratorShared g = *j;
             // deref one more time to print
-            std::cout << "        " << g << std::endl;
+            if (g == NULL) {
+                std::cout << "        " << "<Empty>" << std::endl;                
+            }
+            else {
+                // need to deref the shared generator
+                std::cout << "        " << *g << std::endl;
+            }
         }
     }
 }
-
 
 
 void Generator :: render(FRAME_COUNT_T f) {
@@ -113,18 +117,16 @@ void Generator :: render(FRAME_COUNT_T f) {
 }
 
 
-// this is a critical method in that it both adds a parameter type instance but also sets up the inputs vectors
 void Generator :: _register_input_parameter_type(ParameterTypeShared pts) {
-
     // storing in an unordered map
     _input_parameter_type[_input_parameter_count] = pts;
 
     // put a place holder for the GeneratorShared here; empty
     // does not alloc
     // Note: this makes many copies, but is a very small reference obj
-    std::vector<GeneratorShared> vInner;  
-    vInner.push_back(GeneratorShared());
-    inputs.push_back(vInner);
+    VGeneratorShared vInner;  
+    //vInner.push_back(GeneratorShared());
+    _inputs.push_back(vInner); // extr copy made here
     _input_parameter_count += 1;
 }
 
@@ -152,36 +154,37 @@ void Generator :: set_parameter_by_index(PARAMETER_INDEX_T i,
     // should change
 
     // this removes all stored values
-    inputs[i].clear();
-    inputs[i].push_back(gs);    
+    _inputs[i].clear();
+    _inputs[i].push_back(gs);    
 }
 
+// TODO: create a Constant
 // overridden method for setting a sample value: generates a constant
 void Generator :: set_parameter_by_index(PARAMETER_INDEX_T i, 
                                         SAMPLE_T v){
-    inputs[i].clear();
+    _inputs[i].clear();
     // TODO: create a constant at value v
-    inputs[i].push_back(GeneratorShared());    
+    _inputs[i].push_back(GeneratorShared());    
 }
 
 
 void Generator :: add_parameter_by_index(PARAMETER_INDEX_T i, 
                                         GeneratorShared gs){
-    inputs[i].push_back(gs);    
+    _inputs[i].push_back(gs);    
 }
 
+// TODO: create a Constant
 // overridden method for setting a sample value: adds a constant
-void Generator :: set_parameter_by_index(PARAMETER_INDEX_T i, 
+void Generator :: add_parameter_by_index(PARAMETER_INDEX_T i, 
                                         SAMPLE_T v){
-    // TODO: create a constant at value v
-    inputs[i].push_back(GeneratorShared());    
+    _inputs[i].clear();
+    _inputs[i].push_back(GeneratorShared());    
 }
 
 
 
 //==============================================================================
-Constant :: Constant() 
-    : _value(0) {
+Constant :: Constant() {
     _name = "Constant"; 
     // Generator :: _init auto called
     _init(); // call constants init
@@ -190,45 +193,119 @@ Constant :: Constant()
 Constant :: ~Constant() {
 }
 
+// the int routie must configure the names and types of parameters
 void Constant :: _init() {
     std::cout << *this << ": init()" << std::endl;
     // register some parameters
     aw::ParameterTypeValueShared pt1 = aw::ParameterTypeValueShared(new 
                                        aw::ParameterTypeValue);
     pt1->set_name("Constant numerical value");
-
     // when this is called, the inputs vectors are filled with GeneratorShared
     // these will the inputs vector will be filled with 1 empty placeholder
     _register_input_parameter_type(pt1);
-
+    // by default, add a zero value
+    _values.push_back(0);    
+    
 }
-
-
-// overridden method for setting value
-void Constant :: set_parameter_by_index(PARAMETER_INDEX_T i, 
-                                        SAMPLE_T v){
-    _value = v;    
-    reset(); 
-}
-
-void Constant :: add_parameter_by_index(PARAMETER_INDEX_T i, 
-                                        SAMPLE_T v){
-    // simply add to this value; could store a vector?
-    _value += v;    
-    reset();  
-}
-
 
 void Constant :: reset() {
     std::cout << "calling "<< *this << " reset" << std::endl;
+    // sum values first, then rest all
+    SAMPLE_T v(0);
+    
+    VSample :: const_iterator j;
+    for (j = _values.begin(); j!=_values.end(); ++j) {
+        v += *j;
+    }
     // do not need to partion by diminsons
     for (int i=0; i<_output_size; ++i) {
-        output[i] = _value; // set to the value
+        output[i] = v; // set to the value
     }
     // always reset frame count?
     _frame_count = 0;
 }
 
+void Constant :: print_inputs() {
+    std::cout << *this << " Inputs:" << std::endl;
+    // the size here is correct
+    for (PARAMETER_INDEX_T k=0; k!=_inputs.size(); ++k) {   
+        ParameterTypeShared pts = _input_parameter_type[k];
+        std::cout << "    " << *pts << std::endl;
+        // need to iterate over values in _values
+        VSample :: const_iterator j;
+        for (j = _values.begin(); j!=_values.end(); ++j) {
+            std::cout << "        " << "<Value: " << *j << " >" << std::endl;
+        }
+    }
+}
+
+void Constant :: render(FRAME_COUNT_T f) {
+    // do not need to iterate, as we are always at the necessary value
+    _frame_count = f;
+}
+
+void Constant :: set_parameter_by_index(PARAMETER_INDEX_T i, SAMPLE_T v){
+    _values.clear();
+    _values.push_back(v);    
+    reset(); 
+}
+
+void Constant :: add_parameter_by_index(PARAMETER_INDEX_T i, SAMPLE_T v){
+    _values.push_back(v);    
+    reset();  
+}
+
+
+
+
+
+//==============================================================================
+Add :: Add()
+    : _input_index_num_value(0), _sum(0) {
+    _name = "Add"; 
+    // Generator :: _init auto called
+    _init();
+}
+// dtor
+Add :: ~Add() {
+}
+
+// the int routie must configure the names and types of parameters
+void Add :: _init() {
+    std::cout << *this << ": init()" << std::endl;
+    // register some parameters
+    aw::ParameterTypeValueShared pt1 = aw::ParameterTypeValueShared(new 
+                                       aw::ParameterTypeValue);
+    pt1->set_name("Opperands");
+    _register_input_parameter_type(pt1);
+    _input_index_num_value = 0; // stored for speed
+}
+
+
+void Add :: render(FRAME_COUNT_T f) {
+
+    VGeneratorShared :: const_iterator j; // vector of generators
+
+    while (_frame_count < f) {
+        // for each of the inputs, need to render up to this frame
+        for (j=_inputs[_input_index_num_value].begin(); 
+             j!=_inputs[_input_index_num_value].end(); ++j) {
+            // this is a shared generator
+            (*j)->render(f);
+        }
+        // i is the position in output; assume that all outputs are the same 
+        // size. 
+        for (int i=0; i<_output_size; ++i) {
+            _sum = 0;
+            for (j=_inputs[_input_index_num_value].begin(); 
+                 j!=_inputs[_input_index_num_value].end(); ++j) {
+                _sum += (*j)->output[i];
+            }
+            output[i] = _sum; 
+        }
+        _frame_count += 1;
+    }    
+}
 
 
 } // end namespaces aw
