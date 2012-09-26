a23a205ec58184929a8cb74f40391b946e778b64
flexatone <flexatone@localhost>
2012-09-26 11:27:14 -0700

    preliminary implementation of Add gen

--- aw/src/aw_generator.h
+++ aw/src/aw_generator.h
@@ -23,20 +23,25 @@ namespace aw {
 class ParameterType;
 typedef std::tr1::shared_ptr<ParameterType> ParameterTypeShared;
 class ParameterType {
-    protected:
+    protected://----------------------------------------------------------------
     std::string _name;
     // for specific instances we can define what the rate or sample value means 
     std::string _description; 
 
     // can also define, for a particular instance, an expected context; that is, if fq is required, this can be defined here. then, when given another generator at a different context, conversion can happen?
-    public:    
+	
+    public://-------------------------------------------------------------------
+    explicit ParameterType();
+    virtual ~ParameterType();
 
-    ParameterType();
-    ~ParameterType();
+	//! Output stream processor.
+	friend std::ostream& operator<<(std::ostream& output, 
+						const ParameterType& pt);
 
-    // not passing by const ref because making a copy?
-    void set_name(std::string s) {_name = s;};
-    // should this return a const by reference?
+    //! Set the name of this ParameterType. Passed by const reference as assignment makes the necessary copy. This name is set by the Generator and may not be the same even if its the same ParameterType subclass. 
+    void set_name(const std::string& s) {_name = s;};
+
+    //! Return the name as a string. 
     std::string get_name() {return _name;};
     
 };
@@ -50,76 +55,74 @@ class ParameterTypeValue: public ParameterType {
 
 //==============================================================================
 // todo: add mechanism to tag generators
-
 class Generator;
 typedef std::tr1::shared_ptr<Generator> GeneratorShared;
 class Generator {
-
-    protected:    
+	
+    protected://----------------------------------------------------------------
     std::string _name;
 
     FRAME_DIM_T _output_frame_dimension;
     FRAME_SIZE_T _output_size; // can use frame size, as is 16 bit
     FRAME_SIZE_T _frame_size; // if changed, need to rebuild output
     
-    //! The number of rames that have passed since the last reset
+    //! The number of frames that have passed since the last reset.
     FRAME_COUNT_T _frame_count;
     
-    //! Can store dictionary of paramter type classes: these can store all sorts of specialized metadata data about the inputs needed for this Generator
+    //! The main storage for ParameterTypeShared instances. These are mapped by index value, which is the same index value in the inputs vector. 
     std::tr1::unordered_map<PARAMETER_INDEX_T, 
                             ParameterTypeShared> _input_parameter_type;
     PARAMETER_INDEX_T _input_parameter_count;
 
 
-    public:
+    public://-------------------------------------------------------------------
     typedef std::tr1::unordered_map<PARAMETER_INDEX_T, ParameterTypeShared> MapIndexToParameterTypeShared;
 
+	typedef std::vector<SAMPLE_T> VSample;
     typedef std::vector<GeneratorShared> VGeneratorShared;
     typedef std::vector< VGeneratorShared > VVGeneratorShared;
 
-    //! A std::vector if GeneratorsShared that are the inputs to this function. This remains public so that a client of this Generator can access the input directly form the vector at constant time without a function call. This could be an unordered map too, but vector will have optimal performance when we know the index in advance.
-    // std::vector<GeneratorShared> inputs;
 
-    // or: each required input is actually itself a vector of inputs, meaning that every required input (of a certain parameter type) can have multiple sub-inputs. The inner iterator may be better as a list.
-    VVGeneratorShared inputs;
 
     // this means that inputs are not the same as parameters; each parameter can have multiple inputs
     
     //! A linear array of samples, which may include multiple dimensions in series. This probably should be private, but for performance this is presently public; when configured to run  dimensions can be stored via requests and than used as constants w/o function calls. 
-    // This could be a vector, but optimal performance, no need for sorting, suggests using an dyn alloc array for now.
     SAMPLE_T* output;
 
 
     // methods ================================================================
-    protected:
-    //! Initialize the Generator, allocating the output array.
+    protected://----------------------------------------------------------------
+	
+    //! A std::vector of vectors of GeneratorsShared that are the inputs to this function. This could be an unordered map too, but vector will have optimal performance when we know the index in advance.
+    VVGeneratorShared _inputs;	
+	
+    //! Initialize the Generator. This method is responsible for creating ParameterTypeValueShared instances and adding them to the Generator using the _register_input_parameter_type method. 
     virtual void _init();
 
-    //! Resize the output vector. Always called at creation; can be called when inputs are added
+    //! Resize the output vector. Always called at creation; can be called when inputs are added that have a different output size and folddown is set to a fixed value. 
     void _resize_output();
 
-    // to be called in init routine to setup Generator
+    // Called by Generators during _init() to configure the input parameters found in this Generator. ParameterTypeShared instances are stored in the Generator and the _input_parameter_count is incremented. 
     void _register_input_parameter_type(ParameterTypeShared pts);
 
 
-    public:
-    Generator();
+    public://-------------------------------------------------------------------
+    explicit Generator();
     virtual ~Generator();
 
     //! Reset all parameters, and zero out the output array.
     virtual void reset();
 
-	friend std::ostream &operator<<(std::ostream& output, const Generator& g);
+	friend std::ostream& operator<<(std::ostream& output, const Generator& g);
 
     //! Print the output buffer for all dimensions at the current sample.
     void print_output();
 
-    //! Print the the hierarchical list of all input values.
-    void print_inputs();
+    //! Print the the hierarchical list of all input values. This is virtual because Constant must print inputs in as different way. No other generator should need to specialize. 
+    virtual void print_inputs();
 
-    //! Render the requested frame if not already rendered
-    // should this be passed by const ref?
-    void render(FRAME_COUNT_T f); 
+    //! Render the requested frame if not already rendered. This is virtual because every Generator renders in a different way. 
+    virtual void render(FRAME_COUNT_T f); 
 
     //! Return the number of parameters; this is not the same as the number of Generators, as each parameter may have 1 or more Generators
     PARAMETER_INDEX_T get_parameter_count() {
@@ -146,17 +149,47 @@ class Constant;
 typedef std::tr1::shared_ptr<Constant> ConstantShared;
 class Constant: public Generator {
 
-    private:
-    SAMPLE_T _value;
+    private://------------------------------------------------------------------
+	//! Storage for the internal constant values. This is an array because we want to support a similar interface of applying multiple values to a single input parameter. 
+    VSample _values;
 
-    protected:
+    protected://----------------------------------------------------------------
     virtual void _init();
 
-    public:
-    Constant();
+    public://-------------------------------------------------------------------
+    explicit Constant();
     ~Constant();
+	
+    virtual void reset();
+	
+	//! This derived function is necessary to handle displaying internal input components.
+	virtual void print_inputs();
+	
+	//! This overridden method needs only increment the _frame_count, as the output array is set when reset() is called. 
+    virtual void render(FRAME_COUNT_T f); 	
+	virtual void set_parameter_by_index(PARAMETER_INDEX_T i, SAMPLE_T v);
+	virtual void add_parameter_by_index(PARAMETER_INDEX_T i, SAMPLE_T v);
+};
+
+//==============================================================================
+class Add;
+typedef std::tr1::shared_ptr<Add> AddShared;
+class Add: public Generator {
+
+    private://------------------------------------------------------------------
+    PARAMETER_INDEX_T _input_index_num_value;    
+    SAMPLE_T _sum;
 
 
+    protected://----------------------------------------------------------------
+    virtual void _init();
+
+    public://-------------------------------------------------------------------
+    explicit Add();
+    ~Add();
+		
+	//! Render addition. 
+    virtual void render(FRAME_COUNT_T f); 	
 };
 
 
--- aw/src/aw_generator_test.cpp
+++ aw/src/aw_generator_test.cpp
@@ -1,4 +1,5 @@
 // g++ aw_generator_test.cpp aw_generator.cpp aw_common.cpp -DSTAND_ALONE -lboost_unit_test_framework  -Wall -o aw_generator_test
+
 // -std=c++0x
 
 #define BOOST_TEST_DYN_LINK
@@ -13,7 +14,6 @@
 
 
 BOOST_AUTO_TEST_CASE(aw_generator_test_1) {
-	std::cout << "running aw_generator 1" << std::endl;
 
 	aw::Generator g1;
 	g1.print_output();
@@ -23,29 +23,92 @@ BOOST_AUTO_TEST_CASE(aw_generator_test_1) {
 	g1.print_output();
 
 	aw::GeneratorShared g2 = aw::GeneratorShared(new aw::Generator);
+    BOOST_CHECK_CLOSE(g2->output[0], 0, .0000001);
+	
 	g2->render(4);
 	g2->print_output();
+	// check default
 
 
+}
+
+BOOST_AUTO_TEST_CASE(aw_parameter_type_test_2) {
+	// testing creation
+	aw::ParameterTypeShared pt1 = aw::ParameterTypeShared(new 
+                                    aw::ParameterType);
+
+}
+
+
+BOOST_AUTO_TEST_CASE(aw_generator_constant_test_1) {
 	// can creat the table subclass
 	aw::Constant g3;
     g3.print_inputs();
 
     BOOST_CHECK_EQUAL(g3.get_parameter_count(), 1);
     BOOST_CHECK_EQUAL(g3.get_parameter_index_from_name("Constant numerical value"), 0);
-
+	
+	// set a constant value
+	g3.set_parameter_by_index(0, 29);
+	g3.print_output();
+	g3.render(4);
+	g3.print_output();
 	
 
+    BOOST_CHECK_CLOSE(g3.output[0], 29, .0000001);
+
+	g3.add_parameter_by_index(0, 1);
+	g3.render(4);
+	g3.print_inputs();
+	// this is the sum 
+    BOOST_CHECK_CLOSE(g3.output[0], 30, .0000001);
 }
 
-BOOST_AUTO_TEST_CASE(aw_parameter_type_test_2) {
-	std::cout << "running aw_generator 2" << std::endl;
 
-	// testing creationg
-	aw::ParameterTypeShared pt1 = aw::ParameterTypeShared(new 
-                                    aw::ParameterType);
+
+
+
+
+BOOST_AUTO_TEST_CASE(aw_generator_add_1) {
+	// note: these are all being made of different type
+	// is this correct?
+	aw::GeneratorShared g1 = aw::ConstantShared(new aw::Constant);
+	aw::GeneratorShared g2 = aw::ConstantShared(new aw::Constant);
+	aw::GeneratorShared g3 = aw::AddShared(new aw::Add);
+
+	g1->set_parameter_by_index(0, 2);
+	g2->set_parameter_by_index(0, 3);
+	
+	// need to set first, so as to clear out the old one
+	g3->add_parameter_by_index(0, g1);
+	g3->add_parameter_by_index(0, g2);
+	g3->print_inputs();
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
