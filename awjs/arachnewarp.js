ebba3fc2c6aca88a9196fa69ba054ac978576ebc
flexatone <flexatone@localhost>
2015-07-24 21:14:51 -0700

    added awjs

---
+++ awjs/arachnewarp.js
@@ -0,0 +1,53 @@
+// -----------------------------------------------
+var Env = function(sampling_rate, frame_size) {
+    this.sampling_rate = sampling_rate;
+    this.frame_size = frame_size;
+};
+// constructors
+Env.prototype.get_default_environment = function() {
+    return new Env(44100, 32);
+};
+// methods
+Env.prototype.get_nyquist = function() {
+    return this.sampling_rate * .5;
+};
+
+
+// -----------------------------------------------
+var Gen = function(env) {
+    this.env = env;
+    this.output_count = 1;
+    this.output_size = 0; // count * frame size
+    this.outputs = []; // a list of lists
+};
+
+// methods
+Gen.prototype._resize_outputs = function() {
+    this.output_size = this.output_count * this.env.frame_size;
+    this.outputs = new Array(this.output_count);
+    for (i = 0; i < this.output_count; i++) {
+        this.outputs[i] = new Array(this.env.frame_size);
+    };
+};
+
+Gen.prototype._register_input_parameter_type = function() {
+}
+Gen.prototype._register_output_parameter_type = function() {
+}
+
+
+// -----------------------------------------------
+// tests
+var env1 = new Env(44100, 64);
+// must call this form the environment
+var env2 = Env.prototype.get_default_environment();
+console.log(env1, env1.get_nyquist());
+console.log(env2, env2.get_nyquist());
+
+
+
+g1 = new Gen(env1);
+g1._resize_outputs();
+console.log(g1);
+
+
