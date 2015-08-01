6c2e019ab6985cffdd9b8433af91fec1a0646060
flexatone <flexatone@localhost>
2015-07-31 18:35:11 -0700

    extended implemenation of awjs; awcpp reset _outputs_size in _clear_output_parameter_types

--- awjs/arachnewarp.js
+++ awjs/arachnewarp.js
@@ -7,6 +7,27 @@ var ConnID = Object.freeze({
     Output: 'Output',
 });
 
+var PTypeID = Object.freeze({
+    Switch:'Switch',
+    Value:'Value',
+    Frequency:'Frequency',
+    Duration:'Duration',
+    Phase:'Phase',
+    Channels:'Channels',
+    Trigger:'Trigger',
+    Cycle:'Cycle', // boolean for cycle control
+    LowerBoundary:'LowerBoundary',
+    UpperBoundary:'UpperBoundary',
+    BreakPoints:'BreakPoints', // for slot in breakpoint, wavetable
+    Buffer:'Buffer',
+    Interpolation:'Interpolation',
+    TimeContext:'TimeContext', // rename duration context?
+    RateContext:'RateContext',
+    Modulus:'Modulus', // for counter
+    Direction:'Direction', //
+    BoundaryContext:'BoundaryContext',
+});
+
 // -----------------------------------------------
 var Env = function(sampling_rate, frame_size) {
     this.sampling_rate = sampling_rate;
@@ -23,25 +44,97 @@ Env.prototype.get_nyquist = function() {
 
 // -----------------------------------------------
 var Gen = function(env) {
-    this.env = env;
-    this.output_count = 1;
-    this.output_size = 0; // count * frame size
+    this._env = env;
+
+    // these are set from environment at each init
+    this._frame_size = 0;
+    this._sampling_rate = 0;
+    this._nyquist = 0;
+
+    this._output_count = 0;
+    this._outputs_size = 0; // count * frame size
+
+    // store a parameter type instnace by parameter index
+    this._output_parameter_type = {};
+
+    this._slot_count = 0;
+    this._slots = []; // a list of Gens (do not declare which PIndex to read from, one per position)
+    this._slot_parameter_type = {};
+
+    this._input_count = 0;
+    this._inputs = []; // a list of lists, where each inner list is a pair of Gen, PIndex
+    this._input_parameter_type = {};
+
+    this._render_count = 0;
+
+    this._summed_inputs = [];
+    // the output storage
     this.outputs = []; // a list of lists
+
 };
 
 // methods
+Gen.prototype.init = function() {
+    // base prototype init transfers frame zie
+    this._frame_size = this._env.frame_size;
+    this._sampling_rate = this._env.sampling_rate;
+    this._nyquist = this._sampling_rate / 2;
+
+};
+
 Gen.prototype._resize_outputs = function() {
-    this.output_size = this.output_count * this.env.frame_size;
-    this.outputs = new Array(this.output_count);
-    for (i = 0; i < this.output_count; i++) {
-        this.outputs[i] = new Array(this.env.frame_size);
+    this._outputs_size = this._output_count * this._frame_size;
+    this.outputs = new Array(this._output_count);
+    var i = 0;
+    while (i < this._output_count) {
+        this.outputs[i] = new Array(this._frame_size);
+        i++;
     };
 };
 
-Gen.prototype._register_input_parameter_type = function() {
-}
-Gen.prototype._register_output_parameter_type = function() {
-}
+Gen.prototype.reset = function() {
+    var i = 0;
+    var j = 0;
+    while (i < this._output_count) {
+        while (j < this._frame_size) {
+            this.outputs[i][j] = 0.0;
+            j++;
+        }
+        i++;
+        //window.alert('reset: i:' + i.toString() + ' j: ' + j.toString());
+    }
+    this._render_count = 0;
+
+};
+
+Gen.prototype._register_input_parameter_type = function(ptype) {
+    this._input_parameter_type[this._input_count] = ptype;
+
+    // the added array will store 1 or more pairs of Gen, PIndex
+    this._inputs.push(new Array());
+
+    // this needs to be common frame size, not just this Gen's frame size
+    var vsum = new Array(this._frame_size);
+    // TODO: initialize zeros
+    this._summed_inputs.push(vsum);
+
+    var set_index = this._input_count;
+    this._input_count++;
+    return set_index;
+};
 
 
+Gen.prototype._register_output_parameter_type = function(ptype) {
+    this._output_parameter_type[this._output_count] = ptype;
+    var set_index = this._output_count;
+    this._output_count += 1;
+    this._resize_outputs(); // note: called each time, leading to numerous allocs
+    return set_index;
+};
 
+
+Gen.prototype._clear_output_parameter_types = function() {
+    this._output_count = 0;
+    this._outputs_size = 0;
+    this.outputs = [];
+};
--- awjs/awjs_test.js
+++ awjs/awjs_test.js
@@ -1,4 +1,22 @@
 
+
+
+QUnit.test("test_conn_id", 
+    function(assert) {
+        assert.equal(ConnID.Slot, 'Slot');
+        assert.equal(ConnID.Input, 'Input');
+    }   
+);
+
+
+QUnit.test("test_ptype_id", 
+    function(assert) {
+        assert.equal(PTypeID.Trigger, 'Trigger');
+        assert.equal(PTypeID.RateContext, 'RateContext');
+    }   
+);
+
+
 QUnit.test("test_env_a", 
     function(assert) {
         var env1 = new Env(44100, 64);
@@ -12,13 +30,32 @@ QUnit.test("test_env_a",
 
 QUnit.test("test_gen_resize_a", 
     function(assert) {
-        var env1 = new Env(44100, 64);
-        g1 = new Gen(env1);
+        var env = new Env(44100, 64);
+        g1 = new Gen(env);
+        g1.init();
         g1._resize_outputs();
-        assert.equal(g1.outputs.length, 1);
-        assert.equal(g1.outputs[0].length, env1.frame_size);
+        assert.equal(g1.outputs.length, 0);
+    }   
+);
 
-        console.log(g1);
+
+QUnit.test("test_gen_register_output_parameter_type_a", 
+    function(assert) {
+        var env = new Env(44100, 64);
+        g1 = new Gen(env);
+        g1.init();
+        // replace with PType instances 
+        g1._register_input_parameter_type(PTypeID.Frequency);
+        
+        g1._register_output_parameter_type(PTypeID.Output);
+        g1._register_output_parameter_type(PTypeID.Trigger);
+        assert.equal(g1.outputs.length, 2);
+        g1.reset();
+
+        assert.equal(g1.outputs[0].length, env.frame_size);
+        assert.equal(g1.outputs[1].length, env.frame_size);
+
+        //window.alert(5 + 6);
     }   
 );
 
